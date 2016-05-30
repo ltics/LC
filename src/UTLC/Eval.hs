02c2e194e851a75b4014384a2262f5070ec422a0
zjhmale <zjhmale@gmail.com>
2016-05-30 11:27:14 +0800

    improve eval stratege

--- src/UTLC/Eval.hs
+++ src/UTLC/Eval.hs
@@ -4,10 +4,6 @@ import UTLC.Syntax
 import UTLC.Parser
 import Control.Monad
 
-isValue :: Term -> Bool
-isValue (TmApp _ _) = False
-isValue _  = True
-
 needReduction :: Term -> Bool
 needReduction (TmVar _ _) = False
 needReduction (TmApp _ _) = True
@@ -15,9 +11,8 @@ needReduction (TmAbs _ body) = needReduction body
 
 eval1 :: Term -> Maybe Term
 eval1 (TmApp (TmAbs _ t12) v2) = return $ termSubstTop v2 t12
-eval1 (TmApp t1 t2)
-  | isValue t1 = liftM2 TmApp (return t1) (eval1 t2)
-  | otherwise  = liftM2 TmApp (eval1 t1) (return t2)
+eval1 (TmApp t1@(TmApp _ _) t2) = liftM2 TmApp (eval1 t1) (return t2)
+eval1 (TmApp t1 t2@(TmApp _ _)) = liftM2 TmApp (return t1) (eval1 t2)
 eval1 t@(TmAbs name body) = if needReduction body
                             then liftM (TmAbs name) (eval1 body)
                             else Nothing
