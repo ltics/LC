5795c35aba3f47d133f4a0b5dfcf0a01201f8e26
zjhmale <zjhmale@gmail.com>
2016-06-14 11:50:10 +0800

    finish MT

--- src/MT/Eval.hs
+++ src/MT/Eval.hs
@@ -59,3 +59,125 @@ eval2 env (App e1 e2) = do val1 <- eval2  env e1
                            case val1 of
                              FunVal env' n body -> eval2 (M.insert n val2 env') body
                              _ -> throwError "type error in application"
+
+type Eval3 a = ReaderT Env (ExceptT String Identity) a
+
+runEval3 :: Env -> Eval3 a -> Either String a
+runEval3 env ev = runIdentity (runExceptT (runReaderT ev env))
+
+eval3 :: Exp -> Eval3 Value
+eval3 (Num i) = return $ IntVal i
+eval3 (Var n) = do env <- ask
+                   case M.lookup n env of
+                      Nothing -> throwError ("unbound variable: " ++ n)
+                      Just val -> return val
+eval3 (Add e1 e2) = do e1' <- eval3 e1
+                       e2' <- eval3 e2
+                       case (e1', e2') of
+                         (IntVal i1, IntVal i2) -> return $ IntVal (i1 + i2)
+                         _ -> throwError "type error in addition"
+eval3 (Abs n e) = do env <- ask
+                     return $ FunVal env n e
+eval3 (App e1 e2) = do val1 <- eval3 e1
+                       val2 <- eval3 e2
+                       case val1 of
+                          FunVal env' n body -> local (const (M.insert n val2 env'))
+                                                     (eval3 body)
+                          _ -> throwError "type error in application"
+
+type Eval4 a = ReaderT Env (ExceptT String (StateT Integer Identity)) a
+
+runEval4 :: Env -> Integer -> Eval4 a -> (Either String a, Integer)
+runEval4 env st ev = runIdentity (runStateT (runExceptT (runReaderT ev env)) st)
+
+tick :: (Num s, MonadState s m) => m ()
+tick = do state <- get
+          put (state + 1)
+
+eval4 :: Exp -> Eval4 Value
+eval4 (Num i) = do tick
+                   return $ IntVal i
+eval4 (Var n) = do tick
+                   env <- ask
+                   case M.lookup n env of
+                      Nothing -> throwError ("unbound variable: " ++ n)
+                      Just val -> return val
+eval4 (Add e1 e2) = do tick
+                       e1' <- eval4 e1
+                       e2' <- eval4 e2
+                       case (e1', e2') of
+                         (IntVal i1, IntVal i2) -> return $ IntVal (i1 + i2)
+                         _ -> throwError "type error in addition"
+eval4 (Abs n e) = do tick
+                     env <- ask
+                     return $ FunVal env n e
+eval4 (App e1 e2) = do tick
+                       val1 <- eval4 e1
+                       val2 <- eval4 e2
+                       case val1 of
+                          FunVal env' n body -> local (const (M.insert n val2 env'))
+                                                     (eval4 body)
+                          _ -> throwError "type error in application"
+
+type Eval5 a = ReaderT Env (ExceptT String (WriterT [String] (StateT Integer Identity))) a
+
+runEval5 :: Env -> Integer -> Eval5 a -> ((Either String a, [String]), Integer)
+runEval5 env st ev = runIdentity (runStateT (runWriterT (runExceptT (runReaderT ev env))) st)
+
+eval5 :: Exp -> Eval5 Value
+eval5 (Num i) = do tick
+                   return $ IntVal i
+eval5 (Var n) = do tick
+                   tell [n]
+                   env <- ask
+                   case M.lookup n env of
+                      Nothing -> throwError ("unbound variable: " ++ n)
+                      Just val -> return val
+eval5 (Add e1 e2) = do tick
+                       e1' <- eval5 e1
+                       e2' <- eval5 e2
+                       case (e1', e2') of
+                         (IntVal i1, IntVal i2) -> return $ IntVal (i1 + i2)
+                         _ -> throwError "type error in addition"
+eval5 (Abs n e) = do tick
+                     env <- ask
+                     return $ FunVal env n e
+eval5 (App e1 e2) = do tick
+                       val1 <- eval5 e1
+                       val2 <- eval5 e2
+                       case val1 of
+                          FunVal env' n body -> local (const (M.insert n val2 env'))
+                                                     (eval5 body)
+                          _ -> throwError "type error in application"
+
+type Eval6 a = ReaderT Env (ExceptT String (WriterT [String] (StateT Integer IO))) a
+
+runEval6 :: Env -> Integer -> Eval6 a -> IO ((Either String a, [String]), Integer)
+runEval6 env st ev = runStateT (runWriterT (runExceptT (runReaderT ev env))) st
+
+eval6 :: Exp -> Eval6 Value
+eval6 (Num i) = do tick
+                   liftIO $ print i
+                   return $ IntVal i
+eval6 (Var n) = do tick
+                   tell [n]
+                   env <- ask
+                   case M.lookup n env of
+                      Nothing -> throwError ("unbound variable: " ++ n)
+                      Just val -> return val
+eval6 (Add e1 e2) = do tick
+                       e1' <- eval6 e1
+                       e2' <- eval6 e2
+                       case (e1', e2') of
+                         (IntVal i1, IntVal i2) -> return $ IntVal (i1 + i2)
+                         _ -> throwError "type error in addition"
+eval6 (Abs n e) = do tick
+                     env <- ask
+                     return $ FunVal env n e
+eval6 (App e1 e2) = do tick
+                       val1 <- eval6 e1
+                       val2 <- eval6 e2
+                       case val1 of
+                          FunVal env' n body -> local (const (M.insert n val2 env'))
+                                                     (eval6 body)
+                          _ -> throwError "type error in application"
--- test/MTSpec.hs
+++ test/MTSpec.hs
@@ -13,3 +13,9 @@ spec = describe "evaluation test" $ do
           runEval2 (eval2 M.empty (Num 1 `Add` (Abs "x" (Var "x")))) `shouldBe` Left "type error in addition"
           runEval2 (eval2 M.empty (Var "x")) `shouldBe` Left "unbound variable: x"
           runEval2 (eval2 M.empty (Num 12 `Add` (App (Abs "x" (Var "x")) (Num 4 `Add` Num 2)))) `shouldBe` Right (IntVal 18)
+          runEval3 M.empty (eval3 (Num 12 `Add` (App (Abs "x" (Var "x")) (Num 4 `Add` Num 2)))) `shouldBe` Right (IntVal 18)
+          runEval4 M.empty 0 (eval4 (Num 12 `Add` (App (Abs "x" (Var "x")) (Num 4 `Add` Num 2)))) `shouldBe` (Right (IntVal 18), 8)
+          runEval5 M.empty 0 (eval5 (Num 12 `Add` (App (Abs "x" (Var "x")) (Num 4 `Add` Num 2)))) `shouldBe` ((Right (IntVal 18),["x"]),8)
+          result <- runEval6 M.empty 0 (eval6 (Num 12 `Add` (App (Abs "x" (Var "x")) (Num 4 `Add` Num 2))))
+          result `shouldBe` ((Right (IntVal 18),["x"]),8)
+
